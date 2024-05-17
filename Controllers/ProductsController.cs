36123b564d59ead5cec067af441dbf39fbae4577
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-05-17 15:16:20 +0200

    added products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GrusSidan.Classes;
+using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
 namespace GrusSidan.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly GrusDbContext _context;
+
+        public ProductsController(GrusDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var products = _context.Products.ToList(); // Konvertera till lista
             ViewData["Title"] = "Produkter";
-            return View();
+            return View(products);
         }
+
     }
 }

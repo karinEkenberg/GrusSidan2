508e0ada358fdf933f7afb5eae021f7dda080d14
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-05-17 11:03:45 +0200

    trying to add categories

--- Migrations/20240517090028_AddCategoriesData.Designer.cs
+++ Migrations/20240517090028_AddCategoriesData.Designer.cs
@@ -3,6 +3,7 @@ using GrusSidan.Classes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GrusSidan.Migrations
 {
     [DbContext(typeof(GrusDbContext))]
-    partial class GrusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240517090028_AddCategoriesData")]
+    partial class AddCategoriesData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240517090028_AddCategoriesData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrusSidan.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoriesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO Categories (Name, ImageUrl)
+                VALUES ('Sand', '~/images/sten-kopiera.jpg'),
+                       ('Sten', '~/images/sten-kopiera.jpg'),
+                       ('Grus', '~/images/sten-kopiera.jpg');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM Categories WHERE Name IN ('Sand', 'Sten', 'Grus');
+            ");
+        }
+    }
+}
